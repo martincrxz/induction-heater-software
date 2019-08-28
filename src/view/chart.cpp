b0a3b1ed19b9ffe7f6fdc5647947c090d0c37712
Federico Manuel Gomez Peter <federico.gomez@helpsystems.com>
2019-08-27 21:35:30 -0300

    Agregado wrapper de chart

---
+++ src/view/chart.cpp
@@ -0,0 +1,71 @@
+/*
+ * Created by Federico Manuel Gomez Peter
+ * on 20/8/19.
+ */
+
+#include "chart.h"
+
+#include <QtCharts/QAbstractAxis>
+#include <QtCore/QDateTime>
+#define TIME_CHART_MAX_RANGE 30
+// Tiempo en ms para aceptar un nuevo dato a graficar
+#define TIME_REJECT_DATA 500
+
+Chart::Chart(QGraphicsItem *parent, Qt::WindowFlags wFlags) :
+        QChart(QChart::ChartTypeCartesian, parent, wFlags) {
+    QPen green(Qt::blue);
+    green.setWidth(3);
+    this->series.setPen(green);
+
+    this->addSeries(&this->series);
+    this->legend()->hide();
+
+    this->setTitle("Mediciones del horno");
+
+    this->xAxis.setTickCount(10);
+    this->xAxis.setFormat("HH:mm:ss");
+    this->xAxis.setTitleText("Tiempo");
+
+    this->yAxis.setLabelFormat("%i");
+    this->yAxis.setTitleText("Temperatura (°C)");
+
+    this->addAxis(&this->xAxis, Qt::AlignBottom);
+    QDateTime now = QDateTime::currentDateTime();
+    this->xAxis.setRange(now, now.addSecs(TIME_CHART_MAX_RANGE) );
+    this->series.attachAxis(&this->xAxis);
+    this->addAxis(&this->yAxis, Qt::AlignLeft);
+    this->yAxis.setRange(0, 5);
+    this->series.attachAxis(&this->yAxis);
+
+}
+
+Chart::~Chart() {
+
+}
+
+void Chart::append(double x, double y) {
+    this->series.append(x, y);
+    if (x > this->xAxis.max().toMSecsSinceEpoch()) {
+        this->scroll(25, 0);
+    }
+
+    if (y > this->yAxis.max()) {
+        this->yAxis.setRange(0, y + 10);
+    } else if ( y < this->yAxis.min()) {
+        this->yAxis.setRange(y -10, this->yAxis.max());
+    }
+}
+
+void Chart::init() {
+    QMutexLocker lock(&this->mutex);
+    this->acceptData = true;
+}
+
+void Chart::dataAvailable(double y) {
+    QMutexLocker lock(&this->mutex);
+    if (acceptData && chrono.tack() > TIME_REJECT_DATA) {
+        this->append(chrono.now(), y);
+        chrono.tick();
+    }
+}
+
---
+++ src/view/chart.h
@@ -0,0 +1,41 @@
+/*
+ * Created by Federico Manuel Gomez Peter
+ * on 20/8/19.
+ */
+
+#ifndef __CHART_H__
+#define __CHART_H__
+
+#include <QtCharts/QChart>
+#include <QtCharts/QDateTimeAxis>
+#include <QtCharts/QValueAxis>
+#include <QtCharts/QLineSeries>
+#include <QtCore/QDateTime>
+#include <QtCore/QMutex>
+#include <chrono.h>
+
+QT_CHARTS_USE_NAMESPACE
+
+class Chart :  public QChart {
+    Q_OBJECT
+
+public:
+    explicit Chart(QGraphicsItem *parent = nullptr, Qt::WindowFlags wFlags = nullptr);
+    virtual ~Chart();
+    void init();
+
+private slots:
+    void dataAvailable(double y);
+
+private:
+    void append(double x, double y);
+    QLineSeries series;
+    QDateTimeAxis xAxis;
+    QValueAxis yAxis;
+    QMutex mutex;
+    bool acceptData;
+    Chrono chrono;
+};
+
+
+#endif // __CHART_H__
