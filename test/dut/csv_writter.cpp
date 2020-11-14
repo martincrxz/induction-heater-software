95eca0094e143e1b9e1e90c1612732dd59c87276
Federico Manuel Gomez Peter <fedemgp@gmail.com>
2020-11-14 19:36:15 -0300

    Cambio en el csv writer, ahora muestra porcentaje de potencia

--- test/dut/csv_writter.cpp
+++ test/dut/csv_writter.cpp
@@ -5,11 +5,11 @@
 #include "csv_writter.h"
 
 CsvWritter::CsvWritter(const char *filepath): file(filepath, std::ios_base::out | std::ios_base::trunc) {
-    file << "Potencia (W),Temperatura(°C)" << std::endl;
+    file << "Potencia (%),Temperatura(°C)" << std::endl;
 }
 
 CsvWritter::~CsvWritter() {}
 
 void CsvWritter::writeData(float power, float temp) {
-    file << power << "," << temp << std::endl;
+    file << power * 100 << "," << temp << std::endl;
 }
