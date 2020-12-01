c067f3895e241660cb7d088b3fe4d4a90617868c
Federico Gómez Peter <federico.gomez@helpsystems.com>
2020-12-01 01:18:29 -0300

    Fix del instalador para la versión de QT que estoy usando

--- deploy-installer.bat
+++ deploy-installer.bat
@@ -1,21 +1,23 @@
 set build_dir=%cd%
 echo Setting up environment for Qt usage...
-rem IMPORTANTE, si instala otras versiones de QT, o si se instala este en otra
-rem direcci√≥n, revisar y corregir estos PATHS, o la generaci√≥n del instalador 
-rem no funcionar√°
-set PATH=C:\Qt\5.14.1\mingw73_32\bin;C:\Qt\Tools\mingw730_32\bin;C:\Qt\Tools\QtInstallerFramework\3.2\bin;%PATH%
+@rem IMPORTANTE, si instala otras versiones de QT, o si se instala este en otra
+@rem direcciÛn, revisar y corregir estos PATHS, o la generaciÛn del instalador 
+@rem no funcionar·
+set PATH=C:\Qt\5.15.2\mingw81_32\bin;C:\Qt\Tools\mingw730_32\bin;C:\Qt\Tools\QtInstallerFramework\4.0\bin;%PATH%
 echo Building...
-rem Etapa de compilaci√≥n
+@rem Etapa de compilaciÛn
 cd %build_dir%\build
 qmake.exe ../tprof.pro -spec win32-g++
 mingw32-make -j8
-rem A partir de ac√°, existir√° el ejecutable horno-inducci√≥n.exe, que es el programa final
-rem pero este no podr√° ser ejecutado ya que faltan los .dll de QT
-move horno-induccion.exe ..\deploy\packages\fiuba.solidos.amorfos\data
+@rem A partir de ac·, existir· el ejecutable horno-inducciÛn.exe, que es el programa final
+@rem pero este no podr· ser ejecutado ya que faltan los .dll de QT
+move .\release\horno-induccion.exe ..\deploy\packages\fiuba.solidos.amorfos\data
+mkdir ..\deploy\packages\fiuba.solidos.amorfos\data\fuzzy
+copy ..\fuzzy\* ..\deploy\packages\fiuba.solidos.amorfos\data\fuzzy
 cd ..\deploy\packages\fiuba.solidos.amorfos\data
 windeployqt horno-induccion.exe
-rem a partir de ac√°, el ejecutable se puede ejecutar normalmente. 
-rem Pero se proceder√° a realizar un instalador hecho y derecho
+@rem a partir de ac·, el ejecutable se puede ejecutar normalmente. 
+@rem Pero se proceder· a realizar un instalador hecho y derecho
 cd %build_dir%\deploy
 binarycreator --offline-only -c config/config.xml -p packages horno-induccion-installer
 move horno-induccion-installer.exe %build_dir%
